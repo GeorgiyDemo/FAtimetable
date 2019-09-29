b56512f0dd9d163b6a8010cb77fdfaff848cddf0
Georgiy Demo <demenchuk.george@protonmail.com>
2019-09-29 13:15:54 +0300

    Апдейт шелла

--- docker/autoshell.sh
+++ docker/autoshell.sh
@@ -7,6 +7,7 @@ cp appendonly.aof ./SMS_docker/redis/redis_data/
 cd ./SMS_docker/redis/ && chown -R 1001:1001 redis_data/
 cd redis_data && sudo chown -R 1001:1001 appendonly.aof
 cd ../../
+docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker system prune -a -f
 docker-compose up -d 
