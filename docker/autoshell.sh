ffaf8f980856983cfc95e0dac9bbb3c42371e93a
Georgiy Demo <demenchuk.george@protonmail.com>
2019-09-29 12:31:13 +0300

    Shell переехал

--- docker/autoshell.sh
+++ docker/autoshell.sh
@@ -1,5 +1,5 @@
 #Запускать в папке с SMS_docker
-
+mv docker SMS_docker
 cd ./SMS_docker/redis/redis_data/
 rm *
 cd
@@ -7,7 +7,6 @@ cp appendonly.aof ./SMS_docker/redis/redis_data/
 cd ./SMS_docker/redis/ && chown -R 1001:1001 redis_data/
 cd redis_data && sudo chown -R 1001:1001 appendonly.aof
 cd ../../
-docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker system prune -a -f
 docker-compose up -d 
