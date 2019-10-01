e480bf6859a790c6cc286a5b099e3faf20120e7b
Georgiy Demo <demenchuk.george@protonmail.com>
2019-10-01 11:23:59 +0300

    Каркас telegram-cli по #18

--- docker/telegram-cli/cli.py
+++ docker/telegram-cli/cli.py
@@ -14,8 +14,9 @@ update_id = None
 def main():
     """Запуск бота"""
     global update_id
-    with open("./tokens.yaml", 'r') as stream:
-        token = yaml.load(stream)
+    with open("./settings.yml", 'r') as stream:
+        d = yaml.load(stream)
+    token = d["telegram_token"]
     bot = telegram.Bot(token)
     try:
         update_id = bot.get_updates()[0].update_id
@@ -37,30 +38,32 @@ def handler(bot):
     for update in bot.get_updates(offset=update_id, timeout=10):
         update_id = update.update_id + 1
         
-        if update.message.text == "/start":
+        if update.message.text == "/start" : #AND ЕСТЬ В USER_ID ADMIN
+            print(update.message)
             update.message.reply_text(
                 "Привет 🐾\nЯ помогу тебе следить за конкурсом в списке предзачисления на сайте fa.ru\nДля активации введи команду вида\n<b>/set фамилия имя отчество</b>",
                 parse_mode=p_mode)
 
-        elif update.message.text.split(" ")[0] == "/set":
-            name = update.message.text.split(" ")
+        if update.message.text == "/userid":
+            tg_userid = str(update.message["chat"]["id"])
+            update.message.reply_text(
+                "Ваш userid в Telegram:\n<b>"+tg_userid+"</b>",
+                parse_mode=p_mode)
+            
+        elif update.message.text.split(" ")[0] == "/add":
+            
+            args_list = update.message.text.split(" ")
+            args_list
             # Чтоб ФИО было полное
-            if len(name) != 4:
+            if len(args_list) != 4:
                 update.message.reply_text(
-                    "Что-то пошло не так\nОбщий синтаксис команды:\n<b>/set фамилия имя отчество</b>",
+                    "Что-то пошло не так\nОбщий синтаксис команды:\n<b>/add номер_телефона группа</b>",
                     parse_mode=p_mode)
-
-                return 0
-            name = name[1] + " " + name[2] + " " + name[3]
-            update.message.reply_text("Ищем в списках \"" + name + "\" (может занять некоторое время)")
-            r = requests.post("http://server:5000/adduser",
-                              json={"tid": update.message.from_user.id, "username": name}).json()
-            if r["status"] == "ok":
-                update.message.reply_text(
+            
+            else:
+                 update.message.reply_text(
                     "Ты есть в списках, успешно добавил тебя в систему 😌\nТеперь ты будешь получать уведомления при изменении",
                     parse_mode=p_mode)
-            else:
-                update.message.reply_text("Я не нашел тебя в списках 😔")
 
 
 if __name__ == '__main__':
