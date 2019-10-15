9b4c2bf3d1620a9c7b7da81ba0d42241098121ee
Georgiy Demo <demenchuk.george@protonmail.com>
2019-10-15 18:24:20 +0300

    Инкремент кол-ва отправленных SMS и ошибок для статистики по #23 #22

--- docker/sender/RQExceptionHandler.py
+++ docker/sender/RQExceptionHandler.py
@@ -2,18 +2,21 @@
     Модуль обработки ошибок на уровне rq worker
     Вызывается, если было необработанное исключение в основной программе
 """
-
+import redis
 
 def writer(job, exc_type, exc_value, traceback):
     """
     Метод, вызываемый при ошибке в основной программе
     """
 
+    #Говорим редиске, что у нас  +1 ошибка
+    r_stats = redis.Redis(host='redis', port=6379, decode_responses=True, db=5)
+    r_stats.incr("sms_errors")
+
     # Создаем JSON с данными об ошибке
     ExceptionResult = {
         "status": "exception",
         "description": str(exc_value),
     }
-    print("ОШИБКА")
-    print(ExceptionResult)
+    print("Exception:",ExceptionResult)
     return False
--- docker/sender/sender.py
+++ docker/sender/sender.py
@@ -5,6 +5,7 @@
 
 import time
 import requests
+import redis
 
 class SendSMSClass(object):
     """
@@ -12,6 +13,7 @@ class SendSMSClass(object):
     """
 
     def __init__(self, number, sms, config):
+        self.r_stats = redis.Redis(host='redis', port=6379, decode_responses=True, db=5)
         self.number = number
         self.sms = sms
         self.config = config
@@ -24,7 +26,8 @@ class SendSMSClass(object):
                 requests.get(
                 "http://"+self.config["gsm_url"]+"/SendSMS/user=&password="+self.config["gsm_password"]+"&phoneNumber=" + self.number + "&msg=" + s)
             except:
-                raise Exception('GSM server', 'GSM server exception')
+                raise Exception("GSM server", "GSM server exception")
+            self.r_stats.incr("sms_send")
             time.sleep(1)
 
         time.sleep(self.config["SMS_TIME_SLEEP"])
